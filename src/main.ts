0483abf75f884002c634aa8b8b8e18dcb2cc188c
oxypomme <oxytom8@gmail.com>
2023-03-01 11:55:06 +0100

    added start config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import 'reveal.js/dist/reveal.css';
+
 // see available themes in the
 // node_modules/reveal.js/dist/theme
 //  beige, black, blood, league, moon, night, serif, simple, ...
-import 'reveal.js/dist/theme/sky.css';
-import Reveal from 'reveal.js';
+import 'reveal.js/dist/theme/black.css';
 
-const deck = new Reveal();
+import Reveal from 'reveal.js';
+import RebealMarkdown from 'reveal.js/plugin/markdown/markdown';
+
+const deck = new Reveal({
+  plugins: [RebealMarkdown],
+});
 await deck.initialize({ hash: true, slideNumber: true });
---
+++ src/pages/index.md
@@ -0,0 +1,5 @@
+# Reveal.js + Vite
+
+First slide!
+---
+Second!
