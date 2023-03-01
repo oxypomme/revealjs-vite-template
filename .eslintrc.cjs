665cfbf2f9262c35eb7a41035c049a268cbfbe54
oxypomme <oxytom8@gmail.com>
2023-03-01 11:09:12 +0100

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript-base',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
